c062b5440d3152b2b2a00777f1febc874c6779de
yasingulec <yasin.gulec94@outlook.com>
2022-01-20 00:56:47 +0300

    Generic repository implementation

---
+++ eCommerce.MicroService/Repository/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using eCommerce.MicroService.Data;
+using eCommerce.MicroService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.MicroService.Repository
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private EcommerceDbContext _dbContext;
+        public GenericRepository(EcommerceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<TEntity>()
+                .AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ eCommerce.MicroService/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using eCommerce.MicroService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.MicroService.Repository
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+
+    }
+}
